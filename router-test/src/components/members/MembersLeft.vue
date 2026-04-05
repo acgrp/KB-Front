257081adf82f2ff356103e024276ff35594eacef
acgrp <acgrp103@naver.com>
2026-04-05 15:29:22 +0900

    재설정 커밋

---
+++ router-test/src/components/members/MembersLeft.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="pt-3">
+    <h4>Left</h4>
+    <p>Members</p>
+  </div>
+</template>
+
+<script></script>
+<style></style>
---
+++ workspace/03-2_VueJS/03_testtest/test-vue-app/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App1-1.vue'
+// import CheckboxItem from  './components/CheckboxItem1-0.vue'
+
+
+createApp(App)
+// .component('CheckboxItem', CheckboxItem)
+.mount('#app')
