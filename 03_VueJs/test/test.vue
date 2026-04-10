f421d5189176731881a2bc159948641a4bef80e5
acgrp <acgrp103@naver.com>
2026-04-10 20:25:13 +0900

    커커밋

---
+++ 03_VueJs/test/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// data
+const state = ref(null);
+
+// methods
+const someMethod = () => {
+  
+};
+</script>
+
+<style scoped>
+</style>
